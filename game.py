1fec21de0f9b8cfce181b864b6e5c9e5f495fbf1
Tyler <tyler.quante@gmail.com>
2019-03-14 01:18:02 -0400

    Polishing

--- game.py
+++ game.py
@@ -1,16 +1,21 @@
 from random import randint
 
 game_running = True
-
+game_results = []
 
 while game_running == True:
   new_round = True
+  counter = 0
+
 
   player = { 'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
   monster = { 'name': 'Max', 'attack_min': 10, 'attack_max': 20, 'health': 100}
 
   def calculate_monster_attack():
     return randint(monster['attack_min'], monster['attack_max'])
+  
+  def game_ends(winner_name):
+    print(f'{winner_name} won the game')
 
   print('---' * 10)
   print('Enter Player name')
@@ -20,6 +25,7 @@ while game_running == True:
   print('--- ' + monster['name'] + ' has ' + str(monster['health']) + ' health ---')
 
   while new_round == True:
+    counter += 1
     player_won = False
     monster_won = False
     print('---' * 10)
@@ -27,6 +33,7 @@ while game_running == True:
     print("1) Attack")
     print("2) Heal")
     print("3) Exit Game")
+    print("4) Show Results")
 
     player_choice = input()
 
@@ -54,6 +61,10 @@ while game_running == True:
       game_running = False
       new_round = False  
 
+    elif int(player_choice) == 4:
+      for i in game_results:
+        print(i)
+
     else:
       print("Enter valid input")
 
@@ -62,9 +73,13 @@ while game_running == True:
       print(monster['name'] + ' has ' + str(monster['health']) + ' left')    
 
     elif player_won:
-      print(player['name'] + ' won')
+      game_ends(player['name'])
+      round_result = {'name': player['name'], 'health': player['health'], 'rounds': counter}
+      game_results.append(round_result)
       new_round = False
 
     elif monster_won:
-      print('The monster won')  
+      game_ends(monster['name'])
+      round_result = {'name': player['name'], 'health': player['health'], 'rounds': counter}
+      game_results.append(round_result)
       new_round = False
